e31c6873b7caffbe07f355aea2bd3675e5898d6d
yhonimard <yhoni2103@gmail.com>
2023-10-09 11:48:00 +0700

    feat: add compiler/build project

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   apps: [
-    // {
-    //   name: 'sosmed/api',
-    //   script: './src/index.js',
-    //   interpreter: "./node_modules/.bin/babel-node",
-    //   ignore_watch: ["node_modules", "storage"],
-    //   watch: true,
-    // },
     {
-      name: "sosmed/api",
-      script: "npm",
-      args: "start",
-      cwd: ".",
-      interpreter: "babel-node",
+      name: 'sosmed/api',
+      script: 'src\\index.js',
+      interpreter: "./node_modules/.bin/babel-node",
       ignore_watch: ["node_modules", "storage"],
       watch: true,
-    }
+    },
+    // {
+    //   name: "sosmed/api",
+    //   script: "npm",
+    //   args: "start",
+    //   cwd: "",
+    //   interpreter: "babel-node",
+    //   ignore_watch: ["node_modules", "storage"],
+    //   watch: true,
+    // }
   ],
 };
