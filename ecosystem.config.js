ac86794d2a4af9c08ab9f580b3ced51d7a816d5b
yhonimard <yhoni2103@gmail.com>
2023-10-09 12:45:08 +0700

    feat: update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,33 +3,29 @@ module.exports = {
     {
       name: "sosmed/client",
       script: 'serve',
-      watch: './client-current/dist',
+      watch: true,
+      cwd: "./client-current/dist",
       env: {
-        PM2_SERVE_PATH: './client-current/dist',
+        PM2_SERVE_PATH: '.',
         PM2_SERVE_PORT: 40080,
         PM2_SERVE_SPA: 'true',
       }
     }, {
       name: 'sosmed/api',
-      script: 'npm',
-      args: "run start",
+      script: './api/build/index.js',
       cwd: "./api",
-      watch: "./api",
-      interpreter: "./api/node_modules/.bin/babel-node",
-      ignore_watch: ["./api/node_modules", "./api/storage"],
+      watch: true,
+      ignore_watch: ["node_modules", "./storage"],
     }
   ],
-
-  // deploy: {
-  //   production: {
-  //     user: 'SSH_USERNAME',
-  //     host: 'SSH_HOSTMACHINE',
-  //     ref: 'origin/master',
-  //     repo: 'GIT_REPOSITORY',
-  //     path: 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
+  deploy: {
+    production: {
+      user: 'yhoniserver',
+      host: '[192.168.1.100]',
+      ref: 'origin/main',
+      repo: 'git@github.com:yhonimard/sosmed.git',
+      path: '/home/webserver/sosmed',
+      'post-deploy': 'cd ./api && npm install && npm run build && cd ../client-current && npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+    }
+  }
 };
